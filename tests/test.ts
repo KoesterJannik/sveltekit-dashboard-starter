5bbdb6d70297f2001cf6381e77c56f0446df69f9
Jannik Köster <koesterjannik1998@gmail.com>
2023-12-17 14:01:19 +0100

    added dummy test

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-
+// dummy test
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
